7d5f1c6b7957cbced44bf8d8dd774f9c60d286c4
ReporterNV <reporternv@gmail.com>
2020-10-08 00:49:27 +0700

    rewrite it

--- Lab1.c
+++ Lab1.c
@@ -1,92 +1,66 @@
 #include<stdio.h>
-unsigned char b1 = 0;
-unsigned char b2 = 0;
+#include<unistd.h>
+#include"fun.h"
 
-void out_bin(unsigned char x)	//Закидывает 2ичное значение в перемен
-{
-	printf("%x\n", x);
-	for (int i = 7; i >= 0; i--) {
-		unsigned char out = (x >> i) % 2;
-		printf("%x ", out);
-		b1 += out << i;
-	}
-	puts("");
 
-	puts("");
-	printf("%x", b1);
-	puts("");
-}
-
-void print_bin(unsigned char x)	//Выводит в бинарном формате
-{
-	printf("%x(%c)[%d]\n", x, x, x);
-	for (int i = 7; i >= 0; i--)	//Мб сделать на смещении в условиях на огр и на инкр
-		printf("%x ", (x >> i) % 2);
-	puts("");
-}
-
-unsigned char repeat_decipher()	//Кодирование дублированием
-{
-	unsigned char c = 0;
-	for (int i = 0; i <= 3; i++)
-		c += (b1 >> i * 2) % 2 << 4+i;
-	
-	for (int i = 0; i <= 3; i++)//Переписать со смещением?
-		c += (b2 >> i * 2) % 2 << i;
-	return c;
-}
-
-void repeat(unsigned char x)	//Кодирование дублированием
-{
-	unsigned char out;
-	for (int i = 7; i >= 4; i--) {
-		out = (x >> i) % 2;
-		b1 += out << (2 * i - 7);
-		b1 += out << (2 * i - 8);
-	}
-
-	for (int i = 3; i >= 0; i--) {
-		out = (x >> i) % 2;
-		b2 += out << (2 * i + 1);
-		b2 += out << (2 * i);
-	}
-}
-
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
 	unsigned char x = 0;	//byte 
 	
-	FILE* file;
-	FILE* out;
-	printf("ARG: %s\n", argv[1] );
-	printf("ARG: %s\n", argv[2] );
-	printf("ARG: %s\n", argv[3] );
-	if((file = fopen(argv[1], "r+b")) == 0){
-	printf("Cannot open file:%s\n", argv[1]);
-	return 0;
-	}
-	if((out = fopen(argv[2], "w")) == 0){
-	printf("Cannot create file:%s\n", argv[2]);
-	fclose(file);
-	return 0;
-	}
-	if (!argv[3]){
-	while((fscanf(file, "%hhc", &x)) != EOF){ 
-	if(x == 10){
-		fprintf(out, "%c", 10);
-		continue;}	
-	printf("IN:");
-	print_bin(x);
-//	x = 0b10101010;
-	//b1 = 0b00111111;
-	//b2 = 0b11001100;
+	x = 0b00000000;
+	for(x = 0b00000000;x<=0b11111110;x+=0b00000001){
 	repeat(x);
-	printf("OUT:");
-	print_bin(b1);
-	fprintf(out,"%c",b1);
-	print_bin(b2);
-	fprintf(out,"%c",b2);
-	}}else{
+	printf("%d)", x);
+	bin_out(x);
+	printf(": ");
+	bin_out(b1);
+	printf(" - ");
+	bin_out(b2);
+	printf("\n\n");	}	
+///	printf("%x: %x - %x\n", x, b1, b2);}
+/*	if((b1 == 0b00000000)&&(b2 == 0b00000000))
+		printf("PASS\n");
+	x = 0b00000001;
+	repeat(x);
+	if((b1 == 0b00000000)&&(b2 == 0b00000011))
+		printf("PASS\n");
+*/
+/*
+	FILE *file;
+	FILE *out;
+
+	printf("ARG: %s\n", argv[1]);
+	printf("ARG: %s\n", argv[2]);
+	printf("ARG: %s\n", argv[3]);
+
+	if ((file = fopen(argv[1], "r+b")) == 0) {
+		printf("Cannot open file:%s\n", argv[1]);
+		return 0;
+	}
+	if ((out = fopen(argv[2], "w")) == 0) {
+		printf("Cannot create file:%s\n", argv[2]);
+		fclose(file);
+		return 0;
+	}
+	
+	
+
+	while ((fscanf(file, "%hhc", &x)) != EOF) {
+		if (x == 10) {
+			fprintf(out, "%c", 10);
+			continue;
+		}
+		printf("IN:");
+		print_bin(x);
+		repeat(x);
+		printf("OUT:");
+		print_bin(b1);
+		fprintf(out, "%c", b1);
+		print_bin(b2);
+		fprintf(out, "%c", b2);
+	}
+*/
+/*	
 	while((fscanf(file, "%hhc", &b1)) != EOF){ 
 	if(x == 10){
 		fprintf(out, "%c", 10);
@@ -95,7 +69,8 @@ int main(int argc, char*argv[])
 	repeat_decipher();
 	puts("");
 	}
-}
+*/
+
 	//x= 0b10110101;
 	/*printf("IN:");
 
@@ -112,7 +87,7 @@ int main(int argc, char*argv[])
 	   fprintf(out, "%c", b1);
 	   fprintf(out, "%c", b2);
 	 */
-	fclose(file);
-	fclose(out);
+//	fclose(file);
+//	fclose(out);
 	return 0;
 }
---
+++ fun.h
@@ -0,0 +1,61 @@
+#include<stdio.h>
+unsigned char b1 = 0;
+unsigned char b2 = 0;
+//add getopt
+void bin_out(unsigned char x)	//Выводит в бинарном формате
+{
+	for (int i = 7; i >= 0; i--)	//Мб сделать на смещении в условиях на огр и на инкр
+		printf("%x ", (x >> i) % 2);
+}
+
+void out_bin(unsigned char x)	//Закидывает 2ичное значение в перемен
+{
+	printf("%x\n", x);
+	for (int i = 7; i >= 0; i--) {
+		unsigned char out = (x >> i) % 2;
+		printf("%x ", out);
+		b1 += out << i;
+	}
+	puts("");
+
+	puts("");
+	printf("%x", b1);
+	puts("");
+}
+
+void print_bin(unsigned char x)	//Выводит в бинарном формате
+{
+	printf("%x(%c)[%d]\n", x, x, x);
+	for (int i = 7; i >= 0; i--)	//Мб сделать на смещении в условиях на огр и на инкр
+		printf("%x ", (x >> i) % 2);
+	puts("");
+}
+
+unsigned char repeat_decipher()	//Кодирование дублированием
+{
+	unsigned char c = 0;
+	for (int i = 0; i <= 3; i++)
+		c += (b1 >> i * 2) % 2 << 4 + i;
+
+	for (int i = 0; i <= 3; i++)	//Переписать со смещением?
+		c += (b2 >> i * 2) % 2 << i;
+	return c;
+}
+
+void repeat(unsigned char x)	//Кодирование дублированием
+{
+	unsigned char out;
+	for (int i = 7; i >= 4; i--) {
+		out = (x >> i) % 2;
+		b1 += out << (2 * i - 7);
+		b1 += out << (2 * i - 8);
+	}
+
+	for (int i = 3; i >= 0; i--) {
+		out = (x >> i) % 2;
+		b2 += out << (2 * i + 1);
+		b2 += out << (2 * i);
+	}
+}
+
+
